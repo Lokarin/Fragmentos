f192ee42cbb171451ebd04ea6c975ddbed48041d
Lokarin <49108855+Lokarin@users.noreply.github.com>
2019-10-16 10:13:47 -0300

    Add files via upload

---
+++ dadosdousuario.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+name = input("Insira seu nome: ")
+name2 = input("Insira seu sobrenome completo: ")
+idade = float(input("Insira sua idade: "))
+
+print("Usuário: %s"%name, name2)
+print("Idade: %i"%idade)
+
+if idade < 18:
+   print("Menor de Idade, Permissão Negada")
+else:
+   print("Permissão Concedida")
