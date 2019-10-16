6e18a34fdcd6db7328d01a8450d19774244be8cf
Lokarin <49108855+Lokarin@users.noreply.github.com>
2019-10-16 10:31:28 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+numero = float(input("Número do multiplicando: "))
+
+x1 = numero * 1
+x2 = numero * 2
+x3 = numero * 3
+x4 = numero * 4
+x5 = numero * 5
+x6 = numero * 6
+x7 = numero * 7
+x8 = numero * 8
+x9 = numero * 9
+x10 = numero * 10
+
+print("--- Tabuada do %i ---"%numero )
+print("×1 = %f"%x1)
+print("×2 = %f"%x2)
+print("×3 = %f"%x3)
+print("×4 = %f"%x4)
+print("×5 = %f"%x5)
+print("×6 = %f"%x6)
+print("×7 = %f"%x7)
+print("×8 = %f"%x8)
+print("×9 = %f"%x9)
+print("×10 = %f"%x10)
