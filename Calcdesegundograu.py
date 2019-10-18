49c18821ed57c8d02424d3eefe7874c768f81159
Lokarin <49108855+Lokarin@users.noreply.github.com>
2019-10-17 21:14:58 -0300

    Update Calcdesegundograu.py

--- Calcdesegundograu.py
+++ Calcdesegundograu.py
@@ -13,11 +13,11 @@ if delta < 0:
     print("A equação não possuí solução")
 elif delta == 0:
     x1 = (-valx2+math.sqrt(delta))/2*valx
-    print("Valor de Delta: %f"%delta)
+    print("Valor de Delta: %f \n"%delta)
     print("Resolução: %f"%x1)
 else:
     x1 = (-valx2+math.sqrt(delta))/2*valx
     x2 = (-valx2-math.sqrt(delta))/2*valx
-    print("Valor de Delta: %f"%delta)
+    print("Valor de Delta: %f \n"%delta)
     print("Resolução 1: %f"%x1)
     print("Resolução 2: %f"%x2)
